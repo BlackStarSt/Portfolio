488c69ede5ae57a9c394bc7c3513f500fd421fc5
BlackStarSt <sthefanya86@gmail.com>
2022-06-16 17:20:57 -0300

    Declaração das variáveis do formulario do HTML

---
+++ js/form.js
@@ -0,0 +1,6 @@
+let nome = document.querySelector('#nome');
+let email = document.querySelector('#email');
+let assunto = document.querySelector('#assunto');
+let mensagem = document.querySelector('#mensagem');
+
+//Fazer o curso e depois terminar
